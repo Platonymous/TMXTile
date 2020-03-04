a336b1afb44515dd842cc190827b83be7e17345c
David <david@millionideas.de>
2020-03-04 22:45:01 +0100

    fix argb color

--- src/TMXTile/Map/TMXColor.cs
+++ src/TMXTile/Map/TMXColor.cs
@@ -10,20 +10,35 @@ namespace TMXTile
         public byte G { get; set; }
         public byte B { get; set; }
 
+        public byte A { get; set; } = 255;
+
         public static TMXColor FromString(string value)
         {
-            if (value == null || value.Length < 7)
+            if (value == null || value.Length < 7 || !value.StartsWith("#"))
                 return null;
 
-            byte r = (byte)(Convert.ToUInt32(value.Substring(1, 2), 16));
-            byte g = (byte)(Convert.ToUInt32(value.Substring(3, 2), 16));
-            byte b = (byte)(Convert.ToUInt32(value.Substring(5, 2), 16));
-            return new TMXColor() { R = r, G = g, B = b };
+            if (value.Length == 7)
+            {
+                byte r = (byte)(Convert.ToUInt32(value.Substring(1, 2), 16));
+                byte g = (byte)(Convert.ToUInt32(value.Substring(3, 2), 16));
+                byte b = (byte)(Convert.ToUInt32(value.Substring(5, 2), 16));
+                return new TMXColor() { R = r, G = g, B = b };
+            }else if(value.Length == 9)
+            {
+                byte a = (byte)(Convert.ToUInt32(value.Substring(1, 2), 16));
+                byte r = (byte)(Convert.ToUInt32(value.Substring(3, 2), 16));
+                byte g = (byte)(Convert.ToUInt32(value.Substring(5, 2), 16));
+                byte b = (byte)(Convert.ToUInt32(value.Substring(7, 2), 16));
+
+                return new TMXColor() { R = r, G = g, B = b };
+            }
+
+            return null;
         }
 
         public override string ToString()
         {
-            return "#" + R.ToString("X2") + G.ToString("X2") + B.ToString("X2");
+            return "#"+ (A != 255 ? A.ToString("X2") : "") + R.ToString("X2") + G.ToString("X2") + B.ToString("X2");
         }
         
     }
