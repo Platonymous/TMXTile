6319da55500f5c0f8534bb954b539eda7c4361ae
David <david@millionideas.de>
2020-03-07 23:02:21 +0100

    store transparency color for image layers

--- src/TMXTile/TMXExtensions.cs
+++ src/TMXTile/TMXExtensions.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using xTile;
 using xTile.Dimensions;
 using xTile.Layers;
+using xTile.ObjectModel;
 using xTile.Tiles;
 
 namespace TMXTile
@@ -21,7 +22,10 @@ namespace TMXTile
 
         public static bool IsImageLayer(this Layer layer)
         {
-            return layer.Properties.ContainsKey("@ImageLayer") && layer.Properties["@ImageLayer"] == true;
+            if (layer.Properties.TryGetValue<bool>("@ImageLayer", out PropertyValue value))
+                return value;
+
+            return false;
         }
 
         public static void MakeImageLayer(this Layer layer)
@@ -34,7 +38,7 @@ namespace TMXTile
             if (!layer.IsImageLayer())
                 return null;
 
-            if (layer.Properties.ContainsKey("@ImageLayerTileSheet") && layer.Map.TileSheets.FirstOrDefault(t => t.Id == (string) layer.Properties["@ImageLayerTileSheet"]) is TileSheet ts)
+            if (layer.Properties.TryGetValue<string>("@ImageLayerTileSheet", out PropertyValue value) && layer.Map.TileSheets.FirstOrDefault(t => t.Id == value) is TileSheet ts)
                 return ts;
 
             return null;
@@ -47,44 +51,37 @@ namespace TMXTile
 
         public static Location GetOffset(this Layer layer)
         {
-            return new Location(
-                layer.Properties.ContainsKey("@OffsetX") ? (int) layer.Properties["@OffsetX"] : 0,
-                layer.Properties.ContainsKey("@OffsetY") ? (int) layer.Properties["@OffsetY"] : 0
-                );
+            return layer.Properties.GetOffsetProperties();
         }
 
         public static Location GetOffset(this Tile tile)
         {
-            return new Location(
-                tile.Properties.ContainsKey("@OffsetX") ? (int)tile.Properties["@OffsetX"] : 0,
-                tile.Properties.ContainsKey("@OffsetY") ? (int)tile.Properties["@OffsetY"] : 0
-                );
+            return tile.Properties.GetOffsetProperties();
         }
+
 
         public static void SetOffset(this Layer layer, Location offset)
         {
-            layer.Properties["@OffsetX"] = offset.X;
-            layer.Properties["@OffsetY"] = offset.Y;
+            layer.Properties.SetOffsetProperties(offset);
         }
 
         public static void SetOffset(this Tile tile, Location offset)
         {
-            tile.Properties["@OffsetX"] = offset.X;
-            tile.Properties["@OffsetY"] = offset.Y;
+            tile.Properties.SetOffsetProperties(offset);
         }
 
         public static float GetOpacity(this Layer layer)
         {
-            if(layer.Properties.ContainsKey("@Opacity"))
-                return layer.Properties["@Opacity"];
+            if (layer.Properties.TryGetValue<float>("@Opacity", out PropertyValue value))
+                return value;
 
             return 1f;
         }
 
         public static float GetOpacity(this Tile tile)
         {
-            if (tile.Properties.ContainsKey("@Opacity"))
-                return tile.Properties["@Opacity"];
+            if (tile.Properties.TryGetValue<float>("@Opacity", out PropertyValue value))
+                return value;
 
             return 1f;
         }
@@ -101,26 +98,17 @@ namespace TMXTile
 
         public static TMXColor GetColor(this Layer layer)
         {
-            if (layer.Properties.ContainsKey("@Color"))
-                return TMXColor.FromString(layer.Properties["@Color"]);
-
-            return null;
-        }       
+            return layer.Properties.GetColorFromProperty("@Color");
+        }
 
         public static TMXColor GetColor(this Tile tile)
         {
-            if (tile.Properties.ContainsKey("@Color"))
-                return TMXColor.FromString(tile.Properties["@Color"]);
-
-            return null;
+            return tile.Properties.GetColorFromProperty("@Color");
         }
 
         public static TMXColor GetColor(this Map map)
         {
-            if (map.Properties.ContainsKey("@Color"))
-                return TMXColor.FromString(map.Properties["@Color"]);
-
-            return null;
+            return map.Properties.GetColorFromProperty("@Color");
         }
 
         public static void SetColor(this Layer layer, TMXColor color)
@@ -140,8 +128,8 @@ namespace TMXTile
 
         public static int GetRotationValue(this Tile tile)
         {
-            if (tile.Properties.ContainsKey("@Rotation"))
-                return tile.Properties["@Rotation"];
+            if (tile.Properties.TryGetValue<int>("@Rotation", out PropertyValue value))
+                return value;
 
             return 0;
         }
@@ -153,18 +141,19 @@ namespace TMXTile
 
         public static float GetRotation(this Tile tile)
         {
-            float rotation = tile.GetRotationValue();
+            float value = tile.GetRotationValue();
 
-            if (rotation != 0f)
-                rotation = (float) (Math.PI / (180d / rotation));
+            value %= 360;
+            if (value == 0)
+                return 0;
 
-            return rotation;
+            return (float)(Math.PI / (180.0 / value));
         }
 
         public static int GetFlip(this Tile tile)
         {
-            if (tile.Properties.ContainsKey("@Flip"))
-                return tile.Properties["@Flip"];
+            if (tile.Properties.TryGetValue<int>("@Flip", out PropertyValue value))
+                return value;
 
             return 0;
         }
@@ -177,10 +166,7 @@ namespace TMXTile
 
         public static TMXColor GetBackgroundColor(this Map map)
         {
-            if (map.Properties.ContainsKey("@BackgroundColor"))
-                return TMXColor.FromString(map.Properties["@BackgroundColor"]);
-
-            return null;
+            return map.Properties.GetColorFromProperty("@BackgroundColor");
         }
 
         public static TMXColor SetBackgroundColor(this Map map, TMXColor color)
@@ -188,6 +174,55 @@ namespace TMXTile
             map.Properties["@BackgroundColor"] = color.ToString();
 
             return null;
+        }
+
+        public static TMXColor GetTransparentColor(this TileSheet tilesheet)
+        {
+            return tilesheet.Properties.GetColorFromProperty("@TColor");
+        }
+
+        public static TMXColor GetColorFromProperty(this IPropertyCollection collection, string key)
+        {
+            if (collection.TryGetValue<string>(key, out PropertyValue value))
+                return TMXColor.FromString(value);
+
+            return null;
+        }
+
+        public static Location GetOffsetProperties(this IPropertyCollection collection)
+        {
+            int x = 0;
+            int y = 0;
+
+            if (collection.TryGetValue<int>("@OffsetX", out PropertyValue xValue))
+                x = xValue;
+
+            if (collection.TryGetValue<int>("@OffsetY", out PropertyValue yValue))
+                y = yValue;
+
+            return new Location(
+                x,
+                y
+                );
+        }
+
+        public static void SetOffsetProperties(this IPropertyCollection collection, Location offset)
+        {
+            collection["@OffsetX"] = offset.X;
+            collection["@OffsetY"] = offset.Y;
+        }
+
+        public static bool TryGetValue<T>(this IPropertyCollection collection, string key, out PropertyValue value)
+        {
+            if (collection.TryGetValue(key, out PropertyValue prop)
+                && prop.Type.IsAssignableFrom(typeof(T)))
+            {
+                value = prop;
+                return true;
+            }
+
+            value = null;
+            return false;
         }
 
         public static DrawInstructions GetDrawInstructions(this Tile tile)
